27b9756cb30b26db3f6399c4e1fec2ec6b0ce5c1
Korhan <korhan.bircan@capitalone.com>
2017-05-20 17:40:34 -0700

    Added a UI test to search for the keyword "flower", tap on the first result, change orientation, navigate back to home screen, and delete the keyword.

--- PhotoViewerUITests/PhotoViewerUITests.swift
+++ PhotoViewerUITests/PhotoViewerUITests.swift
@@ -12,25 +12,27 @@ class PhotoViewerUITests: XCTestCase {
         
     override func setUp() {
         super.setUp()
-        
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
+
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         XCUIApplication().launch()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testSearchForFlower() {
+        let app = XCUIApplication()
+        let enterKeywordsSearchField = app.searchFields["Enter keywords"]
+        enterKeywordsSearchField.tap()
+        enterKeywordsSearchField.typeText("flower")
+        app.collectionViews.children(matching: .cell).element(boundBy: 0).children(matching: .other).element.tap()
+        app.otherElements.containing(.navigationBar, identifier:"PhotoViewer.PhotoView").children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .scrollView).element.swipeUp()
+        XCUIDevice.shared().orientation = .landscapeRight
+        XCUIDevice.shared().orientation = .portraitUpsideDown
+        XCUIDevice.shared().orientation = .landscapeLeft
+        XCUIDevice.shared().orientation = .portrait
+        app.navigationBars["PhotoViewer.PhotoView"].buttons["Search Photos"].tap()
+        enterKeywordsSearchField.tap()
+        enterKeywordsSearchField.buttons["Clear text"].tap()
     }
     
 }
